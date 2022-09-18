160a07c678bdc9fbad8aa3c4ac5fc44313352d16
codechallenger122 <77620296+codechallenger122@users.noreply.github.com>
2022-09-18 22:52:19 +0900

    Update dp2_maxSubArray.cpp

--- dp2_maxSubArray.cpp
+++ dp2_maxSubArray.cpp
@@ -1,3 +1,14 @@
+https://leetcode.com/problems/maximum-subarray/
+=====================================================================
+53. Maximum Subarray
+
+Given an integer array nums, 
+find the contiguous subarray (containing at least one number) 
+which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+=====================================================================
+    
 #include <iostream>
 
 using namespace std;
