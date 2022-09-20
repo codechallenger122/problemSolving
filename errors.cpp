d08b323a761834c349579e4f9075053eeda43067
codechallenger122 <77620296+codechallenger122@users.noreply.github.com>
2022-09-20 10:13:23 +0900

    Create errors.cpp

---
+++ errors.cpp
@@ -0,0 +1,4 @@
+# issue1. 22. 09. 20. while solving "unique path 2"
+line 1034 char 9 runtime error reference binding to misaligned address
+--> 이 런타임 에러는 보통 array size 를 넘는 부분을 pointing/referencing 해서 생기는 문제.
+array 에 접근하는 부분을 잘 보길.
