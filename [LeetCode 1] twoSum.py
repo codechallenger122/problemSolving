bfa9bb717329faf534606a81edb4cc218489ce39
codechallenger122 <77620296+codechallenger122@users.noreply.github.com>
2022-09-23 15:54:59 +0900

    Update [LeetCode 1] twoSum.py

--- [LeetCode 1] twoSum.py
+++ [LeetCode 1] twoSum.py
@@ -2,6 +2,7 @@ https://leetcode.com/problems/two-sum/
  
 # n^2 알고리즘 말고, n 알고리즘이 가능하다.
   
+# 1. python 풀이
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dataDict = {}
@@ -11,3 +12,24 @@ class Solution:
             else:
                 dataDict[target-nums[i]] = i
         return [-1,-1]
+
+       
+# 2. C++ 풀이
+class Solution {
+public:
+    unordered_map<int, int> map;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        for(int i=0;i<nums.size();i++) {
+            if(map.count(nums[i])) {
+                res.push_back(map[nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            else {
+                map.insert(make_pair(target-nums[i], i));                
+            }
+        }
+        return res;
+    }
+};
